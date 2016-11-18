f907273f3f31bac5baa60beca333959fee5e5aaa
Brett Minor <brett@Bretts-MacBook-Pro.local>
2016-11-18 10:24:02 -0500

    more changes

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static( __dirname + "/" ) );
 
 app.get('/', function(req, res) {
@@ -12,6 +13,6 @@ app.get('/help', function(req, res) {
 	res.send('Going somewhere?!')
 })
 
-app.listen(80, function() {
-	console.log('Listening on port 8081....');
-})
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
