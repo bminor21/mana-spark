219136895883fd84842d5e798fd0904633e5ec2d
Brett Minor <brett@Bretts-MacBook-Pro.local>
2016-11-18 10:19:23 -0500

    Changed port

--- main.js
+++ main.js
@@ -12,6 +12,6 @@ app.get('/help', function(req, res) {
 	res.send('Going somewhere?!')
 })
 
-app.listen(8081, function() {
+app.listen(80, function() {
 	console.log('Listening on port 8081....');
 })
