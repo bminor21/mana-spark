3e5dc8a6bc646baedb2f1046e52ebccab2f20055
Brett Minor <brett@Bretts-MacBook-Pro.local>
2016-11-19 09:53:19 -0500

    more fixes

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ var app = express();
 var path = require('path');
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static( __dirname + "/" ) );
+//app.use(express.static( __dirname + "/" ) );
 
 app.get('/', function(req, res) {
-	res.sendFile( path.join(__dirname + '/index.html'));
+	res.send("Hello world");//path.join(__dirname + '/index.html'));
 })
 
 app.get('/help', function(req, res) {
