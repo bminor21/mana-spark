cf07b8f6f9fd7b8e93c8d4c1d4a0fded88ed0397
Brett Minor <brett@Bretts-MacBook-Pro.local>
2016-12-10 21:00:41 -0500

    More changes

--- homeCtrl.js
+++ homeCtrl.js
@@ -8,3 +8,13 @@ app.controller("homeCtrl", function($scope){
 	
 	$scope.siteName = "Veltorak.io";
 }); 
+
+app.controller("aboutCtrl", function($scope){
+
+	$scope.aboutMe = [
+	{ question: "What is Veltorak?", answer: "Veltorak is the name of my first World of Warcraft character.\
+	Since then the name has stuck with me and I have used it for a lot of different things." },
+	{ question: "What is the point of this site?", answer : "The whole point of this site is for me to get more\
+	familiar with Node, AngularJS, and web development in general." }
+	];
+});
