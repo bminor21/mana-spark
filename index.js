8fc1d9d806c2400433f164a8abe0bbc447b4e22b
Brett Minor <brett.minor21@gmail.com>
2017-03-08 11:11:04 -0500

    added email link

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/', function(req, res) {
 	res.sendFile( path.join(__dirname + 'index.html'));
 })
 
+app.get('/obkaraoke/', function(req, res ){
+	res.sendFile( path.join( __dirname + 'obkaraoke.php'));
+})
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
