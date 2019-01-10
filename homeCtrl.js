a08208ba1f8a75e3c15a8f782fd799258111ff3c
bminor21 <bminor@bgsu.edu>
2019-01-10 10:41:10 -0500

    Upgrade

--- homeCtrl.js
+++ homeCtrl.js
@@ -1,13 +1,6 @@
 app.controller("homeCtrl", function($scope){
-
-	$scope.navigationItems = [ 
-	{ display : "Home", link : "#home"},
-	{ display : "About", link : "#about" }, 
-	{ display : "Projects", link : "#projects" }
-	];
-	
 	$scope.siteName = "Veltorak.io";
-}); 
+});
 
 app.controller("aboutCtrl", function($scope){
 
